2f3e3cf009be693b79b6b77f320dc6e311d4c2e3
Pinnacle-creator <joerir@gmail.com>
2021-06-25 00:13:00 +0200

    functions klaar

--- functions.js
+++ functions.js
@@ -3,31 +3,57 @@
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Goedemorgen"
 
 function morningGreeter() {
-  return "Goedemorgen";
+    return "Goedemorgen";
 }
 
 const greeting = morningGreeter();
-
 console.log(greeting);
 
 /* Opdracht 1 */
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
+function getSchool() {
+    return "Novi Hogeschool";
+}
 
+const schoolName = getSchool();
+console.log(schoolName);
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function compliment() {
+    return "Lekker bezig met die functies, Jan!"
+}
 
+const sentence = compliment();
+console.log(sentence);
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+function getGrades() {
+    return [8, 8.5, 6, 7]
+}
 
+const array = getGrades();
+console.log(array);
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails() {
+    return credentials
+}
+
+const credentials = {
+    firstName: "Jan",
+    lastName: "Janssen"
+
+}
+const personalInfo = getDetails();
+console.log(personalInfo);
